2dda75db60111473b7076f8a84bcc3c74ff44c88
Madison Evans <m.corbinevans@gmail.com>
2023-01-09 08:28:42 -0700

    commit tree 834fbf5f45795bfe1bc598e4c95aab3be6432bf7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = function (pickup) {
+  return Math.abs(pickup - 42);
+};
+
+const distanceFromHqInFeet = function (pickup) {
+  return Math.abs(pickup - 42) * 264;
+};
+
+const distanceTravelledInFeet = function (start, destination) {
+  return Math.abs(destination - start) * 264;
+};
+
+const calculatesFarePrice = function (start, destination) {
+  if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0;
+  } else if (distanceTravelledInFeet(start, destination) < 2000) {
+    return 0.02 * (distanceTravelledInFeet(start, destination) - 400);
+  } else if (distanceTravelledInFeet(start, destination) < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+};
